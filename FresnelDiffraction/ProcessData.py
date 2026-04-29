d94d84c4eef274e359b62b80c0150638b57111ec
Andrii Goloborodko <a.a.goloborodko@gmail.com>
2026-04-29 14:01:57 +0200

    Apply process executor for the data processin module

--- FresnelDiffraction/ProcessData.py
+++ FresnelDiffraction/ProcessData.py
@@ -1,34 +1,40 @@
-import threading
 import datetime
 import Calculate
 import DataStructures as data
+import os
+import concurrent.futures
+import multiprocessing
 
 
-def fromLine(idx, idl, copy_beam_band, copy_beam, copy_grating, copy_data):
-    idl.start = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    print(f'  Line {idx + 1} processing started at {idl.start}')
-    tmp_beam = idl.beam
-    tmp_grating = idl.grating
-    if copy_beam_band:
-        tmp_beam.band = copy_data.beamband
+def fromLine(idx_line, idl, copy_beam_band, copy_beam, copy_grating, copy_data):
+    if idl.is_ok:
+        idl.start = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        print(f'  Line {idx_line + 1} processing started at {idl.start}')
+        tmp_beam = idl.beam
+        tmp_grating = idl.grating
+        if copy_beam_band:
+            tmp_beam.band = copy_data.beamband
+        else:
+            idl.beam.band = Calculate.beambandSpectrumIntensities(idl.beam)
+            tmp_beam.band = idl.beam.band
+        if copy_grating:
+            tmp_grating.coefficients = copy_data.grating
+        else:
+            idl.grating.coefficients = Calculate.gratingCoefficients(idl.grating, tmp_beam.band.wavelength,
+                                                                     idl.add.accuracy)
+            tmp_grating.coefficients = idl.grating.coefficients
+        if copy_beam:
+            tmp_beam.coefficients = copy_data.beam
+        else:
+            idl.beam.coefficients = Calculate.beamCoefficients(idl.beam, idl.psd.aperture, idl.add.accuracy)
+            tmp_beam.coefficients = idl.beam.coefficients
+        idl.psd.image = Calculate.outputDistribution(tmp_grating, tmp_beam, idl.psd)
+        idl.end = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        print(f'  Line {idx_line + 1} processing finished at {idl.end}')
     else:
-        idl.beam.band = Calculate.beambandSpectrumIntensities(idl.beam)
-        tmp_beam.band = idl.beam.band
-    if copy_grating:
-        tmp_grating.coefficients = copy_data.grating
-    else:
-        idl.grating.coefficients = Calculate.gratingCoefficients(idl.grating, tmp_beam.band.wavelength,
-                                                                 idl.add.accuracy)
-        tmp_grating.coefficients = idl.grating.coefficients
-    if copy_beam:
-        tmp_beam.coefficients = copy_data.beam
-    else:
-        idl.beam.coefficients = Calculate.beamCoefficients(idl.beam, idl.psd.aperture, idl.add.accuracy)
-        tmp_beam.coefficients = idl.beam.coefficients
-    idl.psd.image = Calculate.outputDistribution(tmp_grating, tmp_beam, idl.psd)
-    idl.end = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    print(f'  Line {idx + 1} processing finished at {idl.end}')
-    return idl
+        print(data.text.RED + f'  Line {idx_line + 1} processing skipped since input data is incorrect' +
+              data.text.END)
+    return {idx_line: idl}
 
 
 def fromStructure(ipt):
@@ -40,12 +46,19 @@ def fromStructure(ipt):
     if ipt.copy_beam:
         ipt.copy_data.beam = Calculate.beamCoefficients(ipt.data[0].beam, ipt.data[0].psd.aperture,
                                                         ipt.data[0].add.accuracy)
-    for idx in range(len(ipt.data)):
-        if ipt.data[idx].is_ok:
-            ipt.data[idx] = fromLine(idx, ipt.data[idx], ipt.copy_beam_band, ipt.copy_beam, ipt.copy_grating,
-                                     ipt.copy_data)
-        else:
-            print(data.text.RED + f'  Line {idx + 1} processing skipped since input data is incorrect' + data.text.END)
+
+    ctx = multiprocessing.get_context("spawn")
+    tmp_data = {}
+    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count() - 1, mp_context=ctx) as executor:
+        tmp_data_future = {executor.submit(fromLine, idx, ipt.data[idx], ipt.copy_beam_band, ipt.copy_beam,
+                                           ipt.copy_grating, ipt.copy_data, ) for idx in range(len(ipt.data))}
+        for future in concurrent.futures.as_completed(tmp_data_future):
+            tmp_data.update(future.result())
+    data = []
+    for i in range(len(tmp_data)):
+        data.append(tmp_data[i])
+
+    ipt.data = data
     for idx in range(len(ipt.data)):
         if not ipt.data[idx].is_ok:
             ipt.is_ok = False
--- FresnelDiffraction/TbtCalc.py
+++ FresnelDiffraction/TbtCalc.py
@@ -9,43 +9,52 @@ import DataStructures as data
 import VisualizeData
 import SaveData
 
-dt = datetime.datetime.now()
-cur_dt = dt.strftime("%Y-%m-%d_%H%M")
-Tk().withdraw()
-filenames = askopenfilenames(title='Choose files',
-                             filetypes=(("Excel files", "*.xls*"),
-                                        ("Text files", "*.txt*"),
-                                        ("all files", "*.*")))
-if len(filenames) > 0:
-    wd = os.path.join(os.path.dirname(os.path.realpath(filenames[0])), "results")
-    if not os.path.isdir(wd):
-        os.mkdir(wd)
-    wd = os.path.join(wd, cur_dt)
-    if not os.path.isdir(wd):
-        os.mkdir(wd)
-    for index, filename in enumerate(filenames, start=1):
-        print(data.text.BOLD + f'File {filename}' + data.text.END)
-        st_time = datetime.datetime.now()
-        print(data.text.BOLD + f'File #{index}: calculation started at '
-                               f'{st_time.strftime("%Y/%m/%d %H:%M:%S")}\n' + data.text.END)
-        [is_ok, result] = ReadData.fromFile(wd, filename)
-        if not is_ok:
-            continue
-        result = ProcessData.fromStructure(result)
-        print(f'\n  Data visualization and saving started at {datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}')
-        VisualizeData.images(result)
-        SaveData.fromDatLine(result)
-        result.dependencies = VisualizeData.dependencies(result)
-        SaveData.forDependencies(result)
-        print(f'  Data visualization and saving finished at {datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}\n')
-        end_time = datetime.datetime.now()
-        print(data.text.BOLD + f'File #{index}: calculation finished at '
-                               f'{end_time.strftime("%Y/%m/%d %H:%M:%S")}' + data.text.END)
-        calc_time = end_time - st_time
-        print(data.text.BOLD + f'File #{index}: calculation time is '
-                               f'{calc_time}' + data.text.END)
+if __name__ == "__main__":
+    dt = datetime.datetime.now()
+    cur_dt = dt.strftime("%Y-%m-%d_%H%M")
+    Tk().withdraw()
+    if not os.path.isdir("data_inputs"):
+        filenames = askopenfilenames(title='Choose files',
+                                     filetypes=(("Excel files", "*.xls*"),
+                                                ("Text files", "*.txt*"),
+                                                ("all files", "*.*")))
+    else:
+        dn = os.path.join(os.getcwd(), "data_inputs")
+        filenames = []
+        for file in os.listdir(dn):
+            if file.endswith(".xls") or file.endswith(".xlsx"):
+                filenames.append(os.path.join(dn, file))
 
-        f = open(os.path.join(result.io.filedir, 'result.dat'), 'wb')
-        pickle.dump(result, f, 2)
-        f.close()
-        del result
+    if len(filenames) > 0:
+        wd = os.path.join(os.path.dirname(os.path.realpath(filenames[0])), "results")
+        if not os.path.isdir(wd):
+            os.mkdir(wd)
+        wd = os.path.join(wd, cur_dt)
+        if not os.path.isdir(wd):
+            os.mkdir(wd)
+        for index, filename in enumerate(filenames, start=1):
+            print(data.text.BOLD + f'File {filename}' + data.text.END)
+            st_time = datetime.datetime.now()
+            print(data.text.BOLD + f'File #{index}: calculation started at '
+                                   f'{st_time.strftime("%Y/%m/%d %H:%M:%S")}\n' + data.text.END)
+            [is_ok, result] = ReadData.fromFile(wd, filename)
+            if not is_ok:
+                continue
+            result = ProcessData.fromStructure(result)
+            print(f'\n  Data visualization and saving started at {datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}')
+            VisualizeData.images(result)
+            SaveData.fromDatLine(result)
+            result.dependencies = VisualizeData.dependencies(result)
+            SaveData.forDependencies(result)
+            print(f'  Data visualization and saving finished at {datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}\n')
+            end_time = datetime.datetime.now()
+            print(data.text.BOLD + f'File #{index}: calculation finished at '
+                                   f'{end_time.strftime("%Y/%m/%d %H:%M:%S")}' + data.text.END)
+            calc_time = end_time - st_time
+            print(data.text.BOLD + f'File #{index}: calculation time is '
+                                   f'{calc_time}' + data.text.END)
+
+            f = open(os.path.join(result.io.filedir, 'result.dat'), 'wb')
+            pickle.dump(result, f, 2)
+            f.close()
+            del result
